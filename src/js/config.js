0efa15cc576ac0cbc00ca25717d24231a68f475a
carmapilo <cmpineda@usf.edu>
2024-07-13 13:21:51 -0600

    Fixed upload bug

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'af1848ae-64cb-4d4c-8617-5f6bbc26009c';
+export const KEY = '92b8a1a4-16db-4291-b2dc-5709936bee2d';
 export const MODAL_CLOSE_SEC = 2.5;
